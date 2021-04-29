ab0173aaf97b161af301f9328a652bab8dc2140b
caique almeida <caiquealmeida@pop-os.localdomain>
2021-04-28 22:49:10 -0300

    Created redirect to stripe

---
+++ src/services/stripe-js.ts
@@ -0,0 +1,8 @@
+// stripe for front end
+import { loadStripe} from '@stripe/stripe-js';
+
+export async function getStripeJs() {
+  const stripeJs = await loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY);
+
+  return stripeJs
+}
