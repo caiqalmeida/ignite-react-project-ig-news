43e20cecd917034955caab341e6d47848f2f3ce6
caique almeida <caaguima@gmail.com>
2021-04-30 08:46:47 -0300

    Listing posts

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,10 +1,22 @@
 import Head from "next/head";
 import Prismic from "@prismicio/client";
+import { GetStaticProps } from "next";
+import { RichText } from "prismic-dom";
+
 import { getPrismicClient } from "../../services/prismic";
 import styles from "./styles.module.scss";
-import { GetStaticProps } from "next";
 
-export default function Posts() {
+type Post = {
+  slug: string;
+  title: string;
+  excerpt: string;
+  updatedAt: string;
+};
+interface PostsProps {
+  posts: Post[];
+}
+
+export default function Posts({ posts }: PostsProps) {
   return (
     <>
       <Head>
@@ -13,31 +25,13 @@ export default function Posts() {
 
       <main className={styles.container}>
         <div className={styles.posts}>
-          <a href="">
-            <time>12 de março de 2021</time>
-            <strong>Creating a monorepo with lerna & Yarn Workspaces </strong>
-            <p>
-              In this guide, you will learn how to create a Monorepo to manage
-              multiple packages with a shared
-            </p>
-          </a>
-
-          <a href="">
-            <time>12 de março de 2021</time>
-            <strong>Creating a monorepo with lerna & Yarn Workspaces </strong>
-            <p>
-              In this guide, you will learn how to create a Monorepo to manage
-              multiple packages with a shared
-            </p>
-          </a>
-          <a href="">
-            <time>12 de março de 2021</time>
-            <strong>Creating a monorepo with lerna & Yarn Workspaces </strong>
-            <p>
-              In this guide, you will learn how to create a Monorepo to manage
-              multiple packages with a shared
-            </p>
-          </a>
+          {posts.map((post) => (
+            <a key={post.slug} href="#">
+              <time>{post.updatedAt}</time>
+              <strong>{post.title}</strong>
+              <p>{post.excerpt}</p>
+            </a>
+          ))}
         </div>
       </main>
     </>
@@ -55,9 +49,32 @@ export const getStaticProps: GetStaticProps = async () => {
     }
   );
 
-  console.log(JSON.stringify(response, null, 2));
+  // Instalamos a biblioteca prismic-dom para traduzir o post.data.title já para
+  // um texto
+  // No excerpt, fazemos desta maneira, para conseguimos pegar o primeiro paragrafo
+
+  const posts = response.results.map((post) => {
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      // "Se tiver um paragrafo, você pega o .text dele, se não você retorna texto vazio"
+      excerpt:
+        post.data.content.find((content) => content.type === "paragraph")
+          ?.text ?? "",
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString(
+        "pt-BR",
+        {
+          day: "2-digit",
+          month: "long",
+          year: "numeric",
+        }
+      ),
+    };
+  });
 
   return {
-    props: {},
+    props: {
+      posts,
+    },
   };
 };
