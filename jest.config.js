17520d5970ec44db31fdfbf1fbc691c3dd2b6db9
Caique Almeida <caique.almeida@b2wdigital.com>
2021-08-09 11:40:18 -0300

    Setup tests

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  testIgnorePatterns: ["/node_modules/", "/.next/"],
+  // Local da config
+  setupFilesAfterEnv: ["<rootDir>/src/tests/setupTests.ts"],
+  transform: {
+    // Transformar os arquivos para uma maneira que o jest consiga entender
+    "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest",
+  },
+  // Fala qual ambiente vamos executar
+  testEnvironment: "jsdom",
+};
---
+++ src/tests/setupTests.ts
@@ -0,0 +1,2 @@
+// Trás umas funcionalidades a mais do jest para eu conseguir lidar com a DOM
+import "@testing-library/jest-dom/extend-expect";
